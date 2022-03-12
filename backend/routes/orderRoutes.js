92d93be4e004ae8b74e41759ea8d7b59afc6977a
Rino Arias <rinoariaslaaz@gmail.com>
2022-03-12 03:26:17 -0500

    crear pantalla pedido

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -23,4 +23,18 @@ orderRouter.post(
     res.status(201).send({ message: 'Nueva Orden Creada', order });
   })
 );
+
+orderRouter.get(
+  '/:id',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      res.send(order);
+    } else {
+      res.status(404).send({ message: 'Orden No Encontrada' });
+    }
+  })
+);
+
 export default orderRouter;
